ac7849d470629085491c6e08135088f01e23d364
netoe-DCTEF <elynetobr@outlook.com>
2023-08-09 19:35:27 -0300

    arrumando o script faseDinamica

--- Assets/GerenciaFaseDinamica.cs
+++ Assets/GerenciaFaseDinamica.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Threading.Tasks;
+using UnityEditor.Tilemaps;
 
 /*
  
@@ -66,36 +67,25 @@ public class GerenciaFaseDinamica : MonoBehaviour
     };
 
 
-    int cenarioLmtFazenda = 6;
-    int cenarioLmtArtico = 1;
-    int cenarioLmtEgito = 3;
-    int cenarioLmtEspaco = 1;
-    int cenarioLmtFlorestaFadas = 3;
-    int cenarioLmtMar = 6;
-
-
     // Constantes!
     const string mensagemTutorialTexto = "Selecione a quantidade correta indicada!";
     const string tagInstanciar = "instanciar";
     const string tagNroHud = "nroHud";
     const string tagLogoAnimal = "logoAnimal";
     const string tagTutorialTexto = "tutorialTexto";
-    const string tagBackground = "background";
 
-    //Cosntante default Path's
-
-    const string PATHCENARIOS = "All/Sprites/Cenarios" ;
+    //Constante default Path's
 
 
     // Configuração do HUD
     private GameObject nroHud;
     private GameObject tutorialTexto;
     private GameObject logoAnimal;
-    private GameObject background;
 
     //Parâmetros fase:
     [SerializeField] private string cenaAtualNome;
     [SerializeField] private string cenaProxNome;
+    [SerializeField] private string tipoFase;
 
 
     private int itens_clicarMax = 0;
@@ -106,8 +96,10 @@ public class GerenciaFaseDinamica : MonoBehaviour
     //GameObjects Load
     List<GameObject> itens = new List<GameObject>();
 
-    //Variáveis de acesso externo:
-    private static string cenaProxNome_static;
+    //Variáveis de acesso externo
+
+    public static string cenaProxNome_static { get; set; } 
+
     private static GameObject gameObject_static;
     //public static Text textoHud_static;
 
@@ -117,39 +109,20 @@ public class GerenciaFaseDinamica : MonoBehaviour
         // Reconhecendo os itens sozinho! Algumas fn foram modificadas do script anterior.
         this.loadItensFase();
         //Inicializando qtd e parâmetros
-        itens_clicarMax = sortearQtdMax();
+        itens_clicarMax = sortearQtdMax() ;
         itens_clique = 0;
 
         //Inicializando Objetos para acesso externo.
         cenaProxNome_static = cenaProxNome;
         gameObject_static = this.gameObject;
         itens_clicarMax_static = itens_clicarMax;
+
         // textoHud_static = this.nroHud;
-    }
-
-
-    public static async void passarFase()
-    {
-        ReprodutorSom reprodutor = new ReprodutorSom("Sounds/Geral", gameObject_static);
-        Debug.Log("ItensCLique:" + itens_clique);
-
-        if (itens_clique == itens_clicarMax_static)
-        {
-            await Task.Delay(200);
-            reprodutor.reproduzirArquivo("muito_bem");
-            await Task.Delay(2000);
-            SceneManager.LoadScene(cenaProxNome_static);
-            itens_clique = 0;
-            return;
-        }
-        reprodutor.reproduzirArquivo("tentar_novamente");
-
     }
 
     public static void acrescentarClique()
     {
         ReprodutorSom reprodutor = new ReprodutorSom("Sounds/Geral", gameObject_static);
-
         reprodutor.reproduzirArquivo("sound_acerto");
         itens_clique++;
     }
@@ -162,30 +135,14 @@ public class GerenciaFaseDinamica : MonoBehaviour
 
     private void configurarSprites()
     {
-
-        string ambienteSorteado = sortearAmbiente();
-        string spriteSorteada = sortearSprites(ambienteSorteado);
-        string cenarioSorteado = sortearCenario(ambienteSorteado);
-       
-
-
+        string spriteSorteada = sortearSprites(this.tipoFase);
+ 
         for (int i = 0; i < itens.Count; i++)
         {
-            itens[i].GetComponent<Image>().sprite = Resources.Load<Sprite>("All/Sprites/" + ambienteSorteado + '/' + spriteSorteada.ToLower());
+            itens[i].GetComponent<Image>().sprite = Resources.Load<Sprite>("All/Sprites/Animais/" + this.tipoFase + '/' + spriteSorteada.ToLower());
+           //Debug.LogError("All/Sprites/Animais/" + ambienteSorteado + '/' + spriteSorteada.ToLower());
         }
-
-        background.GetComponent<Image>().sprite = Resources.Load<Sprite>("All/Sprites/" + ambienteSorteado + '/' + cenarioSorteado.ToLower());
         logoAnimal.GetComponent<Image>().sprite = itens[0].GetComponent<Image>().sprite;
-    }
-
-
-    private void adicionandoComponentes()
-    {
-        GameObject[] todosFilhos = GameObject.FindGameObjectsWithTag(tagInstanciar);
-        foreach (GameObject item in todosFilhos)
-        {
-            itens.Add(item);
-        }
     }
 
 
@@ -194,34 +151,23 @@ public class GerenciaFaseDinamica : MonoBehaviour
         nroHud = GameObject.FindGameObjectWithTag(tagNroHud);
         tutorialTexto = GameObject.FindGameObjectWithTag(tagTutorialTexto);
         logoAnimal = GameObject.FindGameObjectWithTag(tagLogoAnimal);   
-        background = GameObject.FindGameObjectWithTag(tagBackground);
 
         //Configurando os gameObjects
         nroHud.GetComponent<Text>().text = itens_clicarMax.ToString();
         tutorialTexto.GetComponent<Text>().text = "Selecione a quantidade correta!";
 
+        GameObject[] todosFilhos = GameObject.FindGameObjectsWithTag(tagInstanciar);
+        foreach (GameObject item in todosFilhos)
+        {
+            itens.Add(item);
+        }
 
-        adicionandoComponentes();
         configurarSprites();
-    }
-
-    private string sortearAmbiente()
-    {
-        System.Random genRand = new System.Random();
-        return ambientesDisponiveis[genRand.Next(0,ambientesDisponiveis.Length - 1)];
     }
     private string sortearSprites(string ambiente)
     {
 
         System.Random genRand = new System.Random();
-        /*
-         "Fazenda",
-        "Artico",
-        "Mar",
-        "Egito",
-        "Espaco"
-         */
-
 
         switch (ambiente)
         {
@@ -237,32 +183,12 @@ public class GerenciaFaseDinamica : MonoBehaviour
                 return espacoClicaveis[genRand.Next(0, espacoClicaveis.Length - 1)];
         }
 
-        return "null";
+        Debug.LogError("A string de ambiente que você colocou não é valida!");
+        return "Erro!";
     }
-
     private int sortearQtdMax()
     {
         System.Random genRand = new System.Random();
         return genRand.Next(1,itens.Count);
-    }
-
-    private string sortearCenario(string ambiente)
-    {
-        System.Random genRand = new System.Random();
-        switch (ambiente)
-        {
-            case "Fazenda":
-                return( PATHCENARIOS + '/' + ambiente + '/' + "cenario" + genRand.Next(1,cenarioLmtFazenda)) ;
-            case "Artico":
-                return (PATHCENARIOS + '/' + ambiente + '/' + "cenario" + genRand.Next(1, cenarioLmtArtico));
-            case "Mar":
-                return (PATHCENARIOS + '/' + ambiente + '/' + "cenario" + genRand.Next(1, cenarioLmtMar));
-            case "Egito":
-                return (PATHCENARIOS + '/' + ambiente + '/' + "cenario" + genRand.Next(1, cenarioLmtEgito));
-            case "Espaco":
-                return (PATHCENARIOS + '/' + ambiente + '/' + "cenario" + genRand.Next(1, cenarioLmtEspaco));
-        }
-
-        return "null";
     }
 }
